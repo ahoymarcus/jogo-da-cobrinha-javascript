a15b2b1145ea9cd25396305691f659f353881ec2
Marcus Vinícius da Silveira Richa <marcus.richa@sempreceub.com>
2021-01-23 00:00:00 -0300

    cria a cobrinha

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ let canvas = document.getElementById("snake");
 */
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+};
+
 
 
 function criarBG() {
@@ -13,11 +19,21 @@ function criarBG() {
     // aqui estamos desenhando o retângulo do jogo
     // Posições: X, Y, Altura, Largura
     context.fillRect(0, 0, 16 * box, 16 * box);
-}
+};
+
+
+function criarCobrinha() {
+    /*será feita com um array: e se mov com acrescentar na 1ª pos e retirar da última pos*/
+    for(i=0; i < snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+};
+
 
 
 criarBG();
-
+criarCobrinha();
 
 
 
