4d928cf6e8f5f01aad6e1083ce1944bf1183f623
Marcus Vinícius da Silveira Richa <marcus.richa@sempreceub.com>
2021-01-25 15:01:11 -0300

    Colocar o movimento na cobrinha e impedir a sua saída da tela do jogo

--- js/script.js
+++ js/script.js
@@ -31,8 +31,35 @@ function criarCobrinha() {
 };
 
 
+// rastreando a cobrinha
+// 37 => direita
+// 38 => para baixo
+// 39 => esquerda
+// 40 => para cima
+document.addEventListener('keydown', update);
+
+function update(event) {
+    
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";   
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+
+
 
 function iniciarJogo() {
+    
+    // lembrando que snake[0] é a cabeça. Aqui, se ele
+    // alcança o fim do lado esq, mudamos sua prop
+    // para ela reaparecer à esq!
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 * box && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+    
+    
     criarBG();
     criarCobrinha();
     
